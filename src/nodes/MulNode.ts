68de1eefa87b1ca42ca10a47fd68349beae59b98
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-17 22:41:19 -0600

    feat: mult c3d add

--- src/nodes/MulNode.ts
+++ src/nodes/MulNode.ts
@@ -4,13 +4,25 @@ import {Cntnr} from "../utils/Cntnr";
 import {Envmnt} from "../utils/Envmnt";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class MulNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendMulti(codeLf.getPointer(), codeRt.getPointer());
+        codeAns.setValue(Multiplicacion(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
+    }
 
     constructor(position: any, lf: Op, rt: Op) {
         super(position);
