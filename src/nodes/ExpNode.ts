fa8d199aafaabb9ba7dd074c7551ab234cc4e9d8
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-17 23:21:15 -0600

    feat: pot c3d add

--- src/nodes/ExpNode.ts
+++ src/nodes/ExpNode.ts
@@ -4,11 +4,11 @@ import {Potencia} from "../utils/AlgebraicOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
+import {Lbl} from "../utils/C3D/Lbl";
 
 export class ExpNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
 
@@ -16,6 +16,32 @@ export class ExpNode extends Op {
         super(position);
         this.lf = lf;
         this.rt = rt;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        const codeControlPot = new Code();
+        codeControlPot.setPointer(Tmp.newTmp());
+        codeControlPot.appendValueToPointer(codeRt.getPointer(), "temporal para control del ciclo de potencia");
+        codeAns.append(codeControlPot);
+        const lbl = Lbl.newLbl();
+        const lblEnd = Lbl.newLbl();
+        codeAns.appendValueToPointer(1);
+        codeAns.appendJE(codeControlPot.getPointer(), "0", lblEnd);
+        codeAns.appendLabel(lbl, "ciclo para realizar potencia");
+        codeAns.appendMulti(codeAns.getPointer(), codeLf.getPointer());
+        const codeControlRef = new Code();
+        codeControlRef.setPointer(codeControlPot.getPointer());
+        codeControlRef.appendResta(codeControlRef.getPointer(), "1");
+        codeAns.append(codeControlRef);
+        codeAns.appendJG(codeControlPot.getPointer(), "0", lbl, "retorna a ciclo para poetencia");
+        codeAns.appendLabel(lblEnd);
+        codeAns.setValue(Potencia(codeLf.getValue(), codeRt.getValue()));
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
