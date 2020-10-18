2c2ca3060ffbdacd8238da10143c21f6d536e8e3
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 01:42:29 -0600

    feat: and c3d add

--- src/nodes/AndNode.ts
+++ src/nodes/AndNode.ts
@@ -4,13 +4,40 @@ import {And} from "../utils/LogicalOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Lbl} from "../utils/C3D/Lbl";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class AndNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const lbl1 = Lbl.newLbl();
+        const lbl2 = Lbl.newLbl();
+        const lbl3 = Lbl.newLbl();
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.appendSplitComment("and start");
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("1");
+        codeAns.appendJE(codeLf.getPointer(), "1", lbl1);
+        codeAns.appendJMP(lbl2);
+        codeAns.appendLabel(lbl1);
+        codeAns.appendJE(codeRt.getPointer(), "1", lbl3);
+        codeAns.appendLabel(lbl2);
+        codeAns.appendValueToPointer("0");
+        codeAns.appendLabel(lbl3);
+        codeAns.appendSplitComment("and end");
+
+        codeAns.setValue(And(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
+    }
 
     constructor(position: any, lf: Op, rt: Op) {
         super(position);
