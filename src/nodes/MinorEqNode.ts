dc8c2933ab06c415bc0b1bc82dad0ea9f2efd491
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 17:06:58 -0600

    feat: mineq c3d add

--- src/nodes/MinorEqNode.ts
+++ src/nodes/MinorEqNode.ts
@@ -5,11 +5,11 @@ import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import {Position} from "../utils/ErrorsControl";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
+import {Lbl} from "../utils/C3D/Lbl";
 
 export class MinorEqNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
 
@@ -17,6 +17,24 @@ export class MinorEqNode extends Op {
         super(position);
         this.lf = lf;
         this.rt = rt;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("1");
+        const lbl = Lbl.newLbl();
+        codeAns.appendJLE(codeLf.getPointer(), codeRt.getPointer(), lbl);
+        codeAns.appendValueToPointer("0");
+        codeAns.appendLabel(lbl);
+
+        codeAns.setValue(MenorEq(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
