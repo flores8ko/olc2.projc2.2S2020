2a96f818d8f1a529d22ce65f178ca3aef161f7ca
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-17 23:25:00 -0600

    feat: sub c3d add

--- src/nodes/SubNode.ts
+++ src/nodes/SubNode.ts
@@ -4,11 +4,10 @@ import {Resta} from "../utils/AlgebraicOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class SubNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
 
@@ -16,6 +15,19 @@ export class SubNode extends Op {
         super(position);
         this.lf = lf;
         this.rt = rt;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        var codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendResta(codeLf.getPointer(), codeRt.getPointer());
+        codeAns.setValue(Resta(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??;
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
