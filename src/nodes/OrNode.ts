64942850ff3d1e6777ce433470f0aebc9b9a3256
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 01:47:14 -0600

    feat: or c3d add

--- src/nodes/OrNode.ts
+++ src/nodes/OrNode.ts
@@ -4,13 +4,35 @@ import {Or} from "../utils/LogicalOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Lbl} from "../utils/C3D/Lbl";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class OrNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const lbl1 = Lbl.newLbl();
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.appendSplitComment("or start");
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("1");
+        codeAns.appendJE(codeLf.getPointer(), "1", lbl1);
+        codeAns.appendJE(codeRt.getPointer(), "1", lbl1);
+        codeAns.appendValueToPointer("0");
+        codeAns.appendLabel(lbl1);
+        codeAns.appendSplitComment("or end");
+
+        codeAns.setValue(Or(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
+    }
 
     constructor(position: any, lf: Op, rt: Op) {
         super(position);
