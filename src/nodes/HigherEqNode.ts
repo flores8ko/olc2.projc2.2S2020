bf0161a584764f9ca98022534adaa4bd2e3fe7cf
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 16:59:11 -0600

    feat: higeq c3d add

--- src/nodes/HigherEqNode.ts
+++ src/nodes/HigherEqNode.ts
@@ -4,11 +4,11 @@ import {MayorEq} from "../utils/RelationalOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
+import {Lbl} from "../utils/C3D/Lbl";
 
 export class HigherEqNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
 
@@ -16,6 +16,24 @@ export class HigherEqNode extends Op {
         super(position);
         this.lf = lf;
         this.rt = rt;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("1");
+        const lbl = Lbl.newLbl();
+        codeAns.appendJGE(codeLf.getPointer(), codeRt.getPointer(), lbl);
+        codeAns.appendValueToPointer("0");
+        codeAns.appendLabel(lbl);
+
+        codeAns.setValue(MayorEq(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
