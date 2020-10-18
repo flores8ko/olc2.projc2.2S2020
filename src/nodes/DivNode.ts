d95573d80d5fa435c00d0967067f14e16a83512d
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-17 18:56:33 -0600

    feat: div c3d add

--- src/nodes/DivNode.ts
+++ src/nodes/DivNode.ts
@@ -4,13 +4,25 @@ import {Division, Suma} from "../utils/AlgebraicOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {Tmp} from "../utils/C3D/Tmp";
+import {GetReferenceValueCode} from "../utils/Utils";
 
 export class DivNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
+
+    public GOCode(env: Envmnt): Code {
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendDiv(codeLf.getPointer(), codeRt.getPointer());
+        codeAns.setValue(Division(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
+    }
 
     constructor(position: any, lf: Op, rt: Op) {
         super(position);
