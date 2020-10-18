4f75b83a0b5d6a2d7edb97bbddef7c84c4b85739
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 17:03:42 -0600

    feat: hig c3d add

--- src/nodes/HigherNode.ts
+++ src/nodes/HigherNode.ts
@@ -4,10 +4,27 @@ import {Mayor} from "../utils/RelationalOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
+import {Lbl} from "../utils/C3D/Lbl";
 
 export class HigherNode extends Op {
     public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
+        const codeLf = GetReferenceValueCode(this.lf.ExeCode(env));
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("1");
+        const lbl = Lbl.newLbl();
+        codeAns.appendJG(codeLf.getPointer(), codeRt.getPointer(), lbl);
+        codeAns.appendValueToPointer("0");
+        codeAns.appendLabel(lbl);
+
+        codeAns.setValue(Mayor(codeLf.getValue(), codeRt.getValue()));
+
+        //TODO tmpmanager ??
+        return codeAns;
     }
     private readonly lf: Op;
     private readonly rt: Op;
