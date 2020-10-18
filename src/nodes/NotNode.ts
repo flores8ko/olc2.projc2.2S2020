42dfee88b8aeee83897e297652c0dad42fd1af81
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-18 01:51:31 -0600

    feat: not c3d add

--- src/nodes/NotNode.ts
+++ src/nodes/NotNode.ts
@@ -4,12 +4,32 @@ import {Not} from "../utils/LogicalOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode} from "../utils/Utils";
+import {Lbl} from "../utils/C3D/Lbl";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class NotNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
+
+    public GOCode(env: Envmnt): Code {
+        const codeRt = GetReferenceValueCode(this.lf.ExeCode(env));
+
+        const lbl1 = Lbl.newLbl();
+
+        const codeAns = new Code(codeRt);
+        codeAns.appendSplitComment("not start");
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer("0");
+        codeAns.appendJE(codeRt.getPointer(), "1", lbl1);
+        codeAns.appendValueToPointer("1");
+        codeAns.appendLabel(lbl1);
+        codeAns.appendSplitComment("not end");
+
+        codeAns.setValue(Not(codeRt.getValue()));
+
+        //TODO tmpmanager ??;
+        return codeAns;
+    }
 
     constructor(position: any, lf: Op, rt: Op) {
         super(position);
