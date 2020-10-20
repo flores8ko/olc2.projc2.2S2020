4b6af29d69eb81a3acf4e0ba07ac0365e4e7e14b
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-20 13:26:28 -0600

    feat: readd c3d add

--- src/nodes/ReAddNode.ts
+++ src/nodes/ReAddNode.ts
@@ -4,16 +4,40 @@ import {Add} from "../utils/AlgebraicOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {GetReferenceValueCode, SemanticException} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
+import {Reference} from "../utils/Reference";
 
 export class ReAddNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
 
     constructor(position: any, lf: Op) {
         super(position);
         this.lf = lf;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const lfVal: object = this.lf.Exe(env);
+        if (!(lfVal instanceof Reference)) {
+            throw new SemanticException(`No se puede asignar a ${(lfVal as Cntnr).typo}, las asignaciones solo pueden ser sobre una referencia`, this.position);
+        }
+
+
+        let codeLf = this.lf.ExeCode(env);
+        const codeLfRefVal = GetReferenceValueCode(codeLf);
+
+        const codeAns = new Code(codeLf, codeLfRefVal);
+        const codeSuma = new Code();
+        codeSuma.setPointer(Tmp.newTmp());
+        codeSuma.appendSuma(codeLfRefVal.getPointer(), "1");
+        codeAns.append(codeSuma);
+        codeAns.appendAsignToStackPosition(codeLf.getPointer(), codeSuma.getPointer());
+
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer(codeLfRefVal.getPointer());
+        codeAns.setValue(Add(codeLf.getValue(), this.position));
+
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
