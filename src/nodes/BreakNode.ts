e7f35e34baf03cc6f9118f56f7ba71210a7d6602
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-20 16:12:27 -0600

    feat: break and continue c3d add

--- src/nodes/BreakNode.ts
+++ src/nodes/BreakNode.ts
@@ -3,14 +3,17 @@ import {Envmnt} from "../utils/Envmnt";
 import {BreakObj} from "./BreakObj";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {BreakObjCode} from "./BreakObjCode";
 
 export class BreakNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     GO(env: Envmnt): object {
         return new BreakObj();
     }
+
+    public GOCode(env: Envmnt): Code {
+        return new BreakObjCode(env.EndLabel);
+    }
+
     GetGraph(env: Envmnt): GraphvizNode {
         return new GraphvizNode('BREAK', [new GraphvizNode('break')]);
     }
--- src/nodes/ContinueNode.ts
+++ src/nodes/ContinueNode.ts
@@ -3,13 +3,15 @@ import {Envmnt} from "../utils/Envmnt";
 import {ContinueObj} from "./ContinueObj";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {ContinueObjCode} from "./ContinueObjCode";
 
 export class ContinueNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     GO(env: Envmnt): object {
         return new ContinueObj();
+    }
+
+    public GOCode(env: Envmnt): Code {
+        return new ContinueObjCode(env.StartLabel);
     }
 
     GetGraph(env: Envmnt): GraphvizNode {
