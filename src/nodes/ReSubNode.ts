6298caddcedf38c47823158f12597cb6642b8fec
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-20 13:28:26 -0600

    feat: resub c3d add

--- src/nodes/ReSubNode.ts
+++ src/nodes/ReSubNode.ts
@@ -1,19 +1,43 @@
 import {Op} from "../utils/Op";
 import {Envmnt} from "../utils/Envmnt";
-import {Sub} from "../utils/AlgebraicOperationsFunctions";
+import {Add, Sub} from "../utils/AlgebraicOperationsFunctions";
 import {Cntnr} from "../utils/Cntnr";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {Reference} from "../utils/Reference";
+import {GetReferenceValueCode, SemanticException} from "../utils/Utils";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class ReSubNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
 
     constructor(position: any, lf: Op) {
         super(position);
         this.lf = lf;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const lfVal: object = this.lf.Exe(env);
+        if (!(lfVal instanceof Reference)) {
+            throw new SemanticException(`No se puede asignar a ${(lfVal as Cntnr).typo}, las asignaciones solo pueden ser sobre una referencia`, this.position);
+        }
+
+
+        let codeLf = this.lf.ExeCode(env);
+        const codeLfRefVal = GetReferenceValueCode(codeLf);
+
+        const codeAns = new Code(codeLf, codeLfRefVal);
+        const codeResta = new Code();
+        codeResta.setPointer(Tmp.newTmp());
+        codeResta.appendResta(codeLfRefVal.getPointer(), "1");
+        codeAns.append(codeResta);
+        codeAns.appendAsignToStackPosition(codeLf.getPointer(), codeResta.getPointer());
+
+        codeAns.setPointer(Tmp.newTmp());
+        codeAns.appendValueToPointer(codeLfRefVal.getPointer());
+        codeAns.setValue(Add(codeLf.getValue(), this.position));
+
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
