3bc4e9db7282a91516861e005be977c8bab85ae8
Fernando Flores <nbafernandoflores8@gmail.com>
2020-10-20 13:49:41 -0600

    feat: reasignadd c3d add

--- src/nodes/ReAsignAddNode.ts
+++ src/nodes/ReAsignAddNode.ts
@@ -1,13 +1,12 @@
 import {Cntnr, Envmnt, Op, Reference} from "../index";
-import {SemanticException} from "../utils/Utils";
+import {GetReferenceValueCode, SemanticException} from "../utils/Utils";
 import {Suma} from "../utils/AlgebraicOperationsFunctions";
 import {GraphvizNode} from "../utils/GraphvizNode";
 import { Code } from "../utils/C3D/Code";
+import {STRING} from "../utils/PrimitiveTypoContainer";
+import {Tmp} from "../utils/C3D/Tmp";
 
 export class ReAsignAddNode extends Op {
-    public GOCode(env: Envmnt): Code {
-        throw new Error("Method not implemented.");
-    }
     private readonly lf: Op;
     private readonly rt: Op;
 
@@ -15,6 +14,44 @@ export class ReAsignAddNode extends Op {
         super(position);
         this.lf = lf;
         this.rt = rt;
+    }
+
+    public GOCode(env: Envmnt): Code {
+        const value = this.GO(env) as Cntnr;
+
+        const codeLfRef = this.lf.ExeCode(env);
+        const codeLf = GetReferenceValueCode(codeLfRef);
+        const codeRt = GetReferenceValueCode(this.rt.ExeCode(env));
+
+        const codeAns = new Code(codeLfRef, codeLf, codeRt);
+        codeAns.setPointer(Tmp.newTmp());
+        if (value instanceof STRING) {
+            codeAns.appendSplitComment("Start String asign");
+            codeAns.appendValueToPointer("H", "string start");
+
+            const codePos = new Code();
+            codePos.setPointer(Tmp.newTmp());
+            codePos.appendSuma(codeAns.getPointer(), 0 + "", "str[" + 0 + "]");
+            let ch = value.getValue().length;
+            codePos.appendAsignToHeapPosition(codePos.getPointer(), ch, "str[" + 0 + "]=" + ch + " (strLth)");
+            codeAns.append(codePos);
+
+            for (let i = 1; i <= value.getValue().length; i++) {
+                const codePos = new Code();
+                codePos.setPointer(Tmp.newTmp());
+                codePos.appendSuma(codeAns.getPointer(), i + "", "str[" + i + "]");
+                ch = value.getValue().charCodeAt(i - 1);
+                codePos.appendAsignToHeapPosition(codePos.getPointer(), ch + "", "str[" + i + "]=" + String.fromCharCode(ch));
+                codeAns.append(codePos);
+            }
+
+            codeAns.appendAddToHeapPointer(value.getValue().length + 1 + "", "string size");
+        }else{
+            codeAns.appendSuma(codeLf.getPointer(), codeRt.getPointer());
+        }
+        codeAns.setValue(value);
+        codeAns.appendAsignToStackPosition(codeLfRef.getPointer(), codeAns.getPointer());
+        return codeAns;
     }
 
     GO(env: Envmnt): object {
